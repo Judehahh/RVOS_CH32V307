3f8fb74c1b9f2bbf980eb782db79d9824f5a6e59
Judehahh <liujh2818@gmail.com>
2023-09-16 22:29:37 +0800

    Add execise 7-1: print "Hello RVOS" in asm

---
+++ exercises/common.mk
@@ -0,0 +1,11 @@
+CROSS_COMPILE = riscv64-unknown-elf-
+CFLAGS = -nostdlib -fno-builtin -march=rv32imac -mabi=ilp32 -g -Wall
+
+OPENOCD_DIR = ../../MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin
+OPENOCD = ${OPENOCD_DIR}/openocd
+CFG = ${OPENOCD_DIR}/wch-riscv.cfg
+
+GDB = gdb-multiarch
+CC = ${CROSS_COMPILE}gcc
+OBJCOPY = ${CROSS_COMPILE}objcopy
+OBJDUMP = ${CROSS_COMPILE}objdump
