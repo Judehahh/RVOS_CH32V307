5d732d6b83110afddc893c72d46ce32e1652ace0
Judehahh <liujh20121601@live.com>
2023-11-19 12:31:22 +0800

    Add support for Archlinux.

--- common.mk
+++ common.mk
@@ -1,7 +1,15 @@
 ifeq ($(shell uname -s),Linux)
-	CROSS_COMPILE := $(shell which riscv64-unknown-elf-gcc > /dev/null 2>&1 && echo riscv64-unknown-elf- || echo riscv64-elf-)
+	ifneq ($(shell which riscv64-unknown-elf-gcc),)
+		CROSS_COMPILE := riscv64-unknown-elf-
+	else
+		CROSS_COMPILE := riscv64-elf-
+	endif
 	OPENOCD_DIR = ../MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin
-	GDB = gdb-multiarch
+	ifneq ($(shell which gdb-multiarch),)
+		GDB := gdb-multiarch
+	else
+		GDB := riscv64-elf-gdb
+	endif
 else ifeq ($(shell uname -s),Darwin)
 	CROSS_COMPILE = riscv64-elf-
 	OPENOCD_DIR = ../MRS_Toolchain_MAC_V190/openocd_x86_64/openocd_x86_64/bin
